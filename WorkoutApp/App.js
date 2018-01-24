2e70e7d046432c510f0103cb8e0f74cfe075d560
Felix Engelbrektsson <ugglan00@gmail.com>
2018-01-24 13:38:56 +0100

    Fixing merge error

--- WorkoutApp/App.js
+++ WorkoutApp/App.js
@@ -6,7 +6,6 @@ export default class App extends React.Component {
     return (
       <View style={styles.container}>
         <Text>Hello World :)</Text>
-        <Text>Heyo asdf</Text>
       </View>
     );
   }
