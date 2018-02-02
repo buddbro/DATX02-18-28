c49e856403cd762229f566f453891ab923917e1b
Felix Engelbrektsson <ugglan00@gmail.com>
2018-02-02 11:49:25 +0100

    Literary can not remember what I have done

---
+++ client/components/Main.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  List,
+  FlatList,
+  ListItem,
+  TextInput
+} from 'react-native';
+import axios from 'axios';
+import LoginMain from './login/LoginMain.js';
+
+export default class Main extends React.Component {
+  constructor(props) {
+    super(props);
+
+  }
+
+  render() {
+    return (
+        <View style={{flex: 1,}}>
+            <LoginMain />
+        </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+
+});
