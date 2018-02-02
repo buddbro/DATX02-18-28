8e0219c7e797c82b57b1f4ceee9d17487860bf1a
Lisa Larsson <lislar@student.chalmers.se>
2018-02-02 14:58:00 +0100

    NewWorkout but not done

--- client/src/containers/AppNavigator.js
+++ client/src/containers/AppNavigator.js
@@ -1,7 +1,7 @@
 import { TabNavigator } from 'react-navigation';
 //import LoginMain from '../components/login/LoginMain';
 import Workout from '../components/Dashboard/Workout.js';
-import WorkoutList from '../components/TempDashboard/WorkoutList.js';
+import WorkoutList from '../components/TempDashboard/WorkoutList';
 import LoginUser from '../components/login/existing/LoginUser';
 import CreateAccount from '../components/login/create/CreateAccount';
 
