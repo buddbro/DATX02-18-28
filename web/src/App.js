b8515502dd806babeebcab570bb5e6a32298ff15
ryggan <andreas@carlssonfoto.se>
2018-02-03 17:54:26 +0100

    Add initial web UI

---
+++ web/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Button from 'material-ui/Button';
+
+class App extends React.Component {
+  render() {
+    return (
+      <Button raised color="primary">
+        Get PushApp
+      </Button>
+    );
+  }
+}
+
+export default App;
