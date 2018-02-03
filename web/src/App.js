139c0080c57815ebc6dfae7a9ed0abfd6d724585
ryggan <andreas@carlssonfoto.se>
2018-02-03 18:44:36 +0100

    Feedback form

--- web/src/App.js
+++ web/src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import Button from 'material-ui/Button';
+import FeedbackForm from './components/FeedbackForm';
 
 class App extends React.Component {
   render() {
     return (
-      <Button raised color="primary">
-        Get PushApp
-      </Button>
+      <div>
+        <Button raised color="primary">
+          Get PushApp
+        </Button>
+        <FeedbackForm />
+      </div>
     );
   }
 }
