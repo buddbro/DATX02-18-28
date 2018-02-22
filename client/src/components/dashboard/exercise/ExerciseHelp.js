a77ce5fbe5bd27519bf15dfb6099c94220c11b2d
Felix Engelbrektsson <ugglan00@gmail.com>
2018-02-22 16:44:28 +0100

    Forgot to merge

--- client/src/components/dashboard/exercise/ExerciseHelp.js
+++ client/src/components/dashboard/exercise/ExerciseHelp.js
@@ -1,35 +1,41 @@
-import React from 'react';
+import React from "react";
 import {
   StyleSheet,
   View,
   Text,
   TouchableOpacity,
-} from 'react-native';
-import {connect} from 'react-redux';
-import NavigationActions from 'react-navigation';
+  TouchableWithoutFeedback
+} from "react-native";
+import { connect } from "react-redux";
+import NavigationActions from "react-navigation";
 
 class ExerciseHelp extends React.Component {
   constructor(props) {
     super(props);
-
   }
 
   render() {
-    return(
+    return (
       <View style={styles.background}>
-        <View style={styles.container}>
+        <TouchableOpacity
+          onPress={() => {
+            this.props.navigation.dispatch(
+              NavigationActions.NavigationActions.navigate({
+                routeName: "ViewExercise"
+              })
+            );
+          }}
+          style={{ width: "100%", height: "100%", zIndex: -9999 }}
+        />
+        <View
+          style={styles.container}
+          disabled={true}
+          onPress={() => {
+            console.log("inner");
+          }}
+        >
           <View style={styles.header}>
             <Text style={styles.title}>{this.props.description.name}</Text>
-            <TouchableOpacity
-              onPress={() => {
-                this.props.navigation.dispatch(
-                  NavigationActions.NavigationActions.navigate({
-                    routeName: 'ViewExercise'
-                  })
-                );
-              }}>
-              <Text>Back</Text>
-            </TouchableOpacity>
           </View>
           <View style={styles.body}>
             <Text>{this.props.description.description}</Text>
@@ -39,40 +45,40 @@ class ExerciseHelp extends React.Component {
     );
   }
 }
-const mapStateToProps = ({app}) => {
-  return {description: app.exerciseHelp}
-}
+const mapStateToProps = ({ app }) => {
+  return { description: app.exerciseHelp };
+};
 
-export default connect(mapStateToProps, {
-})(ExerciseHelp);
+export default connect(mapStateToProps, {})(ExerciseHelp);
 
 const styles = StyleSheet.create({
   background: {
-    backgroundColor: 'rgba(0, 0, 0, 0.3)',
-    width: '100%',
-    height: '100%',
-    zIndex: -999,
+    backgroundColor: "rgba(102, 105, 203, 0.7)",
+    width: "100%",
+    height: "100%",
+    zIndex: -999
   },
   container: {
-    backgroundColor: 'white',
+    position: "absolute",
+    backgroundColor: "#B9BAF1",
     borderRadius: 3,
-    marginTop: '25%',
-    marginBottom: '25%',
-    marginRight: '10%',
-    marginLeft: '10%',
+    marginTop: "25%",
+    marginBottom: "25%",
+    marginRight: "10%",
+    marginLeft: "10%",
     flex: 1,
-    flexDirection: 'column',
+    flexDirection: "column"
   },
   header: {
-    flexDirection: 'row',
-    flex: 1,
+    flexDirection: "row",
+    flex: 1
   },
   title: {
-    fontWeight: 'bold',
-    fontSize: 24,
+    fontWeight: "bold",
+    fontSize: 24
   },
   body: {
     flex: 4,
-    flexDirection: 'column',
-  },
+    flexDirection: "column"
+  }
 });
