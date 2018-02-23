acd712d3a01bdf0110657542f938a026d658ade0
ryggan <andreas@carlssonfoto.se>
2018-02-23 07:41:49 +0100

    Finishing up workout refactor on backend

--- server/api/queries.js
+++ server/api/queries.js
@@ -62,44 +62,31 @@ const logout = (req, res, next) => userQueries.logout(req, res, next, db);
 const sendResetPasswordEmail = (req, res, next) =>
   user.sendResetPasswordEmail(req, res, next, db);
 
-const getWorkouts = (req, res, next) =>
-  workouts.getWorkouts(req, res, next, db);
-
+const getWorkouts = (req, res, next) => workout.getWorkouts(req, res, next, db);
 const getWorkoutsForUser = (req, res, next) =>
-  workouts.getWorkoutsForUser(req, res, next, db);
-
+  workout.getWorkoutsForUser(req, res, next, db);
 const getWorkoutWithId = (req, res, next) =>
-  workouts.getWorkoutWithId(req, res, next, db);
-
+  workout.getWorkoutWithId(req, res, next, db);
 const getSetsForExercise = (req, res, next) =>
   workout.getSetsForExercise(req, res, next, db);
-
 const addWorkout = (req, res, next) => workout.addWorkout(req, res, next, db);
+const deleteWorkout = (req, res, next) =>
+  workout.deleteWorkout(req, res, next, db);
+const editWorkout = (req, res, next) => workout.editWorkout(req, res, next, db);
+const addExerciseToWorkout = (req, res, next) =>
+  workout.addExerciseToWorkout(req, res, next, db);
+const addSetToExercise = (req, res, next) =>
+  workout.addSetToExercise(req, res, next, db);
 
-const deleteWorkout = (req, res, next) => {
+const fetchExerciseList = (req, res, next) => {
   db
     .any(
-      'SELECT exercises.id AS id FROM workouts, users, exercises WHERE workouts.userId = $1 AND users.token = $2 AND workouts.id = $3 AND exercises.workout = $3',
-      [req.body.id, req.body.token, req.params.id]
+      `SELECT exercise_types.id AS id, name, title AS exercise_type
+          FROM exercise_types, exercise_sections
+          WHERE exercise_types.section = exercise_sections.id
+          ORDER BY section
+        `
     )
-    .then(function(data) {
-      db
-        .any('DELETE FROM workouts WHERE id = $1', [req.params.id])
-        .then(function() {
-          res.status(200).json({ success: true });
-        });
-    })
-    .catch(function(err) {
-      return next(err);
-    });
-};
-
-const editWorkout = (req, res, next) => {
-  db
-    .any('UPDATE workouts SET title = $1 WHERE id = $2', [
-      req.body.title,
-      req.params.id
-    ])
     .then(function(data) {
       res.status(200).json(data);
     })
@@ -133,88 +120,17 @@ const postFeedback = (req, res, next) => {
     });
 };
 
-const fetchExerciseList = (req, res, next) => {
-  db
-    .any(
-      `SELECT exercise_types.id AS id, name, title AS exercise_type
-        FROM exercise_types, exercise_sections
-        WHERE exercise_types.section = exercise_sections.id
-        ORDER BY section
-      `
-    )
-    .then(function(data) {
-      res.status(200).json(data);
-    })
-    .catch(function(err) {
-      return next(err);
-    });
-};
-
 const fetchExerciseDescription = (req, res, next) => {
   db
     .any(
       `SELECT name, description
-        FROM exercise_types
-        WHERE id = $1
-      `,
+          FROM exercise_types
+          WHERE id = $1
+        `,
       [req.params.id]
     )
     .then(function(data) {
       res.status(200).json(data);
-    })
-    .catch(function(err) {
-      return next(err);
-    });
-};
-
-const addExerciseToWorkout = (req, res, next) => {
-  const { workoutId, exerciseId } = req.body;
-  db
-    .any('INSERT INTO exercises(exercise_type, workout) VALUES($1, $2)', [
-      exerciseId,
-      workoutId
-    ])
-    .then(function() {
-      db
-        .any(
-          `SELECT exercises.id AS id, exercise_types.name AS title
-            FROM exercises, exercise_types
-            WHERE exercise_types.id = exercises.exercise_type
-            ORDER BY id DESC
-            LIMIT 1
-          `
-        )
-        .then(function(data) {
-          res.status(200).json(data);
-        });
-    })
-    .catch(function(err) {
-      return next(err);
-    });
-};
-
-const addSetToExercise = (req, res, next) => {
-  const { userId, token, reps, weight } = req.body;
-  const { id } = req.params;
-
-  db
-    .any('INSERT INTO sets(exercise, reps, weight) VALUES($1, $2, $3)', [
-      Number(id),
-      Number(reps),
-      Number(weight)
-    ])
-    .then(function(data) {
-      db
-        .any(
-          `SELECT id
-            FROM sets
-            ORDER BY id DESC
-            LIMIT 1
-          `
-        )
-        .then(function(data) {
-          res.status(200).json(data);
-        });
     })
     .catch(function(err) {
       return next(err);
--- server/api/queries/workout.js
+++ server/api/queries/workout.js
@@ -128,10 +128,100 @@ const addWorkout = (req, res, next, db) => {
     });
 };
 
+const deleteWorkout = (req, res, next, db) => {
+  db
+    .any(
+      'SELECT exercises.id AS id FROM workouts, users, exercises WHERE workouts.userId = $1 AND users.token = $2 AND workouts.id = $3 AND exercises.workout = $3',
+      [req.body.id, req.body.token, req.params.id]
+    )
+    .then(function(data) {
+      db
+        .any('DELETE FROM workouts WHERE id = $1', [req.params.id])
+        .then(function() {
+          res.status(200).json({ success: true });
+        });
+    })
+    .catch(function(err) {
+      return next(err);
+    });
+};
+
+const editWorkout = (req, res, next, db) => {
+  db
+    .any('UPDATE workouts SET title = $1 WHERE id = $2', [
+      req.body.title,
+      req.params.id
+    ])
+    .then(function(data) {
+      res.status(200).json(data);
+    })
+    .catch(function(err) {
+      return next(err);
+    });
+};
+
+const addExerciseToWorkout = (req, res, next, db) => {
+  const { workoutId, exerciseId } = req.body;
+  db
+    .any('INSERT INTO exercises(exercise_type, workout) VALUES($1, $2)', [
+      exerciseId,
+      workoutId
+    ])
+    .then(function() {
+      db
+        .any(
+          `SELECT exercises.id AS id, exercise_types.name AS title
+            FROM exercises, exercise_types
+            WHERE exercise_types.id = exercises.exercise_type
+            ORDER BY id DESC
+            LIMIT 1
+          `
+        )
+        .then(function(data) {
+          res.status(200).json(data);
+        });
+    })
+    .catch(function(err) {
+      return next(err);
+    });
+};
+
+const addSetToExercise = (req, res, next, db) => {
+  const { userId, token, reps, weight } = req.body;
+  const { id } = req.params;
+
+  db
+    .any('INSERT INTO sets(exercise, reps, weight) VALUES($1, $2, $3)', [
+      Number(id),
+      Number(reps),
+      Number(weight)
+    ])
+    .then(function(data) {
+      db
+        .any(
+          `SELECT id
+            FROM sets
+            ORDER BY id DESC
+            LIMIT 1
+          `
+        )
+        .then(function(data) {
+          res.status(200).json(data);
+        });
+    })
+    .catch(function(err) {
+      return next(err);
+    });
+};
+
 module.exports = {
   getWorkouts,
   getWorkoutsForUser,
   getWorkoutWithId,
   getSetsForExercise,
-  addWorkout
+  addWorkout,
+  deleteWorkout,
+  editWorkout,
+  addExerciseToWorkout,
+  addSetToExercise
 };
