6ab28beae3800e10cea3a3edca9ed0060c6c78ef
Kaffemakarn <micsod@student.chalmers.se>
2018-02-26 12:33:19 +0100

    Removed the bloody icons!

--- web/src/components/TopMenu.js
+++ web/src/components/TopMenu.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Segment, Dropdown, Menu, Container, Icon } from 'semantic-ui-react';
+import { Segment, Dropdown, Menu, Container, Modal, Image, Header, Button, Form, Icon } from 'semantic-ui-react';
 
 class TopMenu extends React.Component {
   state = { activeItem: 'pushapp' };
@@ -62,42 +62,68 @@ class TopMenu extends React.Component {
               Feedback
             </Menu.Item>
             <Menu.Menu position="right">
-              <Menu.Item
-                name="signin"
-                active={activeItem === 'signin'}
-                onClick={this.handleItemClick}
-              >
-                <Icon name="sign in" />
-                Sign In
-              </Menu.Item>
-              <Menu.Item
-                name="signup"
-                active={activeItem === 'signup'}
-                onClick={this.handleItemClick}
-              >
+              <Dropdown item text={<span><Icon name='sign in'/>Sign In</span>}>
+                <Dropdown.Menu>
+                  <Dropdown.Item>
+                  <Form>
+                    <Form.Field>
+                      <label>Email</label>
+                      <input placeholder='example@gmail.com' style={{ width: '250px' }}/>
+                    </Form.Field>
+                    <Form.Field>
+                      <label>Password</label>
+                      <input placeholder='********' />
+                    </Form.Field>
+                    <Button type='Sign In'>Submit</Button>
+                  </Form>
+                  </Dropdown.Item>
+                </Dropdown.Menu>
+              </Dropdown>
+
+              <Modal trigger={
+                <Menu.Item
+                  name="signup"
+                  active={activeItem === 'signup'}
+                  onClick={this.handleItemClick}
+                >
                 <Icon name="signup" />
-                Sign Up
-              </Menu.Item>
+                  Sign Up
+                </Menu.Item>
+              } size="mini">
+                <Modal.Header>Sign Up</Modal.Header>
+                <Modal.Content>
+                  <Modal.Description>
+                    <Form>
+                      <Form.Field>
+                        <label>Name</label>
+                        <input placeholder='ex. Emilia' />
+                      </Form.Field>
+                      <Form.Field>
+                        <label>Display name</label>
+                        <input placeholder='ex. theTerminator93' />
+                      </Form.Field>
+                      <Form.Field>
+                        <label>Email</label>
+                        <input placeholder='example@gmail.com' />
+                      </Form.Field>
+                      <Form.Field>
+                        <label>Password</label>
+                        <input placeholder='********' />
+                      </Form.Field>
+                      <Form.Field>
+                        <label>Confirm Password</label>
+                        <input placeholder='********' />
+                      </Form.Field>
+                      <Button type='Sign In'>Submit</Button>
+                    </Form>
+                  </Modal.Description>
+                </Modal.Content>
+              </Modal>
             </Menu.Menu>
-            {/* <Menu.Item as="a" to="/" active>
-              Home
-            </Menu.Item> */}
-            {/* <Menu.Item as="a">Work</Menu.Item>
-            <Menu.Item as="a">Company</Menu.Item>
-            <Menu.Item as="a">Careers</Menu.Item>
-            <Menu.Item position="right">
-              <Button as="a" inverted>
-                Log in
-              </Button>
-              <Button
-                as="a"
-                inverted
-                primary
-                style={{ marginLeft: '0.5em' }}
-              >
-                Sign Up
-              </Button>
-            </Menu.Item> */}
+
+
+
+
           </Container>
         </Menu>
       </Segment>
